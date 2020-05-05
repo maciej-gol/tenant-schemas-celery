940c218d296a9419eace62d216c2e8c89bebbc99
Neil Patel <neilpatel@gmail.com>
2020-05-05 15:10:24 +0530

    Issue #28: allow tenant_cache_seconds to be set globally

--- tenant_schemas_celery/task.py
+++ tenant_schemas_celery/task.py
@@ -19,7 +19,7 @@ class TenantTask(Task):
 
     abstract = True
 
-    tenant_cache_seconds = 0
+    tenant_cache_seconds = None
 
     @classmethod
     def tenant_cache(cls):
@@ -32,7 +32,12 @@ class TenantTask(Task):
         missing = object()
         cache = cls.tenant_cache()
         cached_value = cache.get(schema_name, default=missing)
-        tenant_cache_seconds = cls.tenant_cache_seconds or cls._get_app().conf.task_tenant_cache_seconds or 0
+        tenant_cache_seconds = cls.tenant_cache_seconds
+        if tenant_cache_seconds is None: # if not set at task level
+            try: # to get from global setting
+                tenant_cache_seconds = cls._get_app().conf.task_tenant_cache_seconds
+            except AttributeError as e:
+                tenant_cache_seconds = 0 # default
 
         if cached_value is missing:
             cached_value = get_tenant_model().objects.get(schema_name=schema_name)
