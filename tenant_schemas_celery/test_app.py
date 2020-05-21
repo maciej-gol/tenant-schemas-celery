b6ef27d8c581e77c6b6c60cbab6da9904c31da33
awaazde <tech@awaaz.de>
2020-05-21 10:41:30 +0530

    Issue #28/PR #38: resolves https://github.com/maciej-gol/tenant-schemas-celery/pull/38#discussion_r428155261
    (cherry picked from commit 28cd3ebd75661c93ea9b28726cae273d758f8bc7)

--- tenant_schemas_celery/test_app.py
+++ tenant_schemas_celery/test_app.py
@@ -10,7 +10,6 @@ else:
         CELERY_RESULT_BACKEND = 'rpc://'
         CELERY_RESULT_PERSISTENT = False
         CELERY_ALWAYS_EAGER = False
-        CELERY_TASK_TENANT_CACHE_SECONDS = os.environ.get("TASK_TENANT_CACHE_SECONDS", 10)
 
     app.config_from_object(CeleryConfig)
     app.autodiscover_tasks(['tenant_schemas_celery'], 'test_tasks')
