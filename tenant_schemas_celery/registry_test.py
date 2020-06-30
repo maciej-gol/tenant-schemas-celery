a3790fa86b960753c9135448c73186e98a4eecac
Gordon Pendleton <wgordonw1@gmail.com>
2020-06-30 17:06:10 -0400

    adjusted reference to app

--- tenant_schemas_celery/registry_test.py
+++ tenant_schemas_celery/registry_test.py
@@ -3,13 +3,13 @@ import inspect
 from tenant_schemas_celery.task import TenantTask
 from tenant_schemas_celery.app import CeleryApp
 
+from .test_app import app
 from .test_tasks import get_schema_name
 
 
 def test_get_schema_name_registration(transactional_db):
-    # Celery is not able to pick up settings without this
-    app = CeleryApp('testapp')
-    app.config_from_object('django.conf:settings', namespace="CELERY")
+    name = 'tenant_schemas_celery.test_tasks.get_schema_name'
+    assert name in app._tasks
     
     name = 'tenant_schemas_celery.test_tasks.get_schema_name'
     assert name in app._tasks
