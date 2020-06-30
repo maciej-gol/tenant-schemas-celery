aa86ddc8e04dcdc22d01211edfc89387b35fe76a
Gordon Pendleton <wgordonw1@gmail.com>
2020-06-30 17:32:51 -0400

    tested registration of get_schema_from_class_task

--- tenant_schemas_celery/registry_test.py
+++ tenant_schemas_celery/registry_test.py
@@ -4,7 +4,7 @@ from tenant_schemas_celery.task import TenantTask
 from tenant_schemas_celery.app import CeleryApp
 
 from .test_app import app
-from .test_tasks import get_schema_name
+from .test_tasks import get_schema_name, get_schema_from_class_task, SchemaClassTask
 
 
 def test_get_schema_name_registration(transactional_db):
@@ -15,3 +15,14 @@ def test_get_schema_name_registration(transactional_db):
     assert not inspect.isclass(task)
     assert task.__class__.__name__ == 'get_schema_name'
     assert isinstance(task, TenantTask)
+
+
+def test_get_schema_from_class_task_registration(transactional_db):
+    name = 'tenant_schemas_celery.test_tasks.get_schema_from_class_task'
+    assert name in app._tasks
+    
+    task = app._tasks[name]
+    assert not inspect.isclass(task)
+    assert task.__class__.__name__ == 'get_schema_from_class_task'
+    assert isinstance(task, TenantTask)
+    assert isinstance(task, SchemaClassTask)
