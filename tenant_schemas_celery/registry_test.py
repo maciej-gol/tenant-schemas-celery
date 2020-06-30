03580bb2b2ba75106894a099f52a8c13175d80e5
Gordon Pendleton <wgordonw1@gmail.com>
2020-06-30 17:46:30 -0400

    adjusted base class assertions

--- tenant_schemas_celery/registry_test.py
+++ tenant_schemas_celery/registry_test.py
@@ -1,5 +1,6 @@
 import inspect
 
+from celery import Task
 from celery.task import Task as LegacyTask
 
 from tenant_schemas_celery.task import TenantTask
@@ -10,6 +11,9 @@ from .test_tasks import get_schema_name, get_schema_from_class_task, SchemaClass
 
 
 def test_get_schema_name_registration(transactional_db):
+    assert not issubclass(get_schema_name, LegacyTask)
+    assert issubclass(get_schema_name, Task)
+    
     name = 'tenant_schemas_celery.test_tasks.get_schema_name'
     assert name in app._tasks
     
@@ -20,6 +24,9 @@ def test_get_schema_name_registration(transactional_db):
 
 
 def test_get_schema_from_class_task_registration(transactional_db):
+    assert not issubclass(get_schema_from_class_task, LegacyTask)
+    assert issubclass(get_schema_from_class_task, Task)
+    
     name = 'tenant_schemas_celery.test_tasks.get_schema_from_class_task'
     assert name in app._tasks
     
@@ -32,7 +39,7 @@ def test_get_schema_from_class_task_registration(transactional_db):
 
 def test_schema_class_legacy_task_registration(transactional_db):
     assert issubclass(SchemaClassLegacyTask, LegacyTask)
-    assert not issubclass(SchemaClassLegacyTask, TenantTask)
+    assert not issubclass(SchemaClassLegacyTask, Task)
     
     name = 'tenant_schemas_celery.test_tasks.SchemaClassLegacyTask'
     assert name in app._tasks
