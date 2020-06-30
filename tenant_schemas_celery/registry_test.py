7bb9e71ba64efd253499fae086c167c0a0191129
Gordon Pendleton <wgordonw1@gmail.com>
2020-06-30 17:36:36 -0400

    added legacy task registration test

--- tenant_schemas_celery/registry_test.py
+++ tenant_schemas_celery/registry_test.py
@@ -1,10 +1,12 @@
 import inspect
 
+from celery.task import Task as LegacyTask
+
 from tenant_schemas_celery.task import TenantTask
 from tenant_schemas_celery.app import CeleryApp
 
 from .test_app import app
-from .test_tasks import get_schema_name, get_schema_from_class_task, SchemaClassTask
+from .test_tasks import get_schema_name, get_schema_from_class_task, SchemaClassTask, SchemaClassLegacyTask
 
 
 def test_get_schema_name_registration(transactional_db):
@@ -26,3 +28,17 @@ def test_get_schema_from_class_task_registration(transactional_db):
     assert task.__class__.__name__ == 'get_schema_from_class_task'
     assert isinstance(task, TenantTask)
     assert isinstance(task, SchemaClassTask)
+
+
+def test_schema_class_legacy_task_registration(transactional_db):
+    assert issubclass(SchemaClassLegacyTask, LegacyTask)
+    assert not issubclass(SchemaClassLegacyTask, TenantTask)
+    
+    name = 'tenant_schemas_celery.test_tasks.SchemaClassLegacyTask'
+    assert name in app._tasks
+    
+    task = app._tasks[name]
+    assert not inspect.isclass(task)
+    assert task.__class__.__name__ == 'SchemaClassLegacyTask'
+    assert isinstance(task, TenantTask)
+    assert isinstance(task, SchemaClassLegacyTask)
