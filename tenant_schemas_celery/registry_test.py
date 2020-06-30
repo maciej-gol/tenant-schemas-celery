cde163556167243f53effd120757bc1e80332eb8
Gordon Pendleton <wgordonw1@gmail.com>
2020-06-30 17:21:36 -0400

    adjusted expectations and printed registry

--- tenant_schemas_celery/registry_test.py
+++ tenant_schemas_celery/registry_test.py
@@ -10,12 +10,9 @@ from .test_tasks import get_schema_name
 def test_get_schema_name_registration(transactional_db):
     name = 'tenant_schemas_celery.test_tasks.get_schema_name'
     assert name in app._tasks
-    
-    assert inspect.isclass(get_schema_name)
-    assert issubclass(get_schema_name, TenantTask)
+    print(app._tasks)
     
     task = app._tasks[name]
     assert not inspect.isclass(task)
-    assert isinstance(task, TenantTask)
-    assert isinstance(task, get_schema_name)
     assert task.__class__.__name__ == 'get_schema_name'
+    assert isinstance(task, TenantTask)
