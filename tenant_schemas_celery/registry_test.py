410a4592440bece8f95f0c7bb96422d3fd81853a
Gordon Pendleton <wgordonw1@gmail.com>
2020-06-30 17:30:18 -0400

    removed print

--- tenant_schemas_celery/registry_test.py
+++ tenant_schemas_celery/registry_test.py
@@ -10,7 +10,6 @@ from .test_tasks import get_schema_name
 def test_get_schema_name_registration(transactional_db):
     name = 'tenant_schemas_celery.test_tasks.get_schema_name'
     assert name in app._tasks
-    print(app._tasks)
     
     task = app._tasks[name]
     assert not inspect.isclass(task)
