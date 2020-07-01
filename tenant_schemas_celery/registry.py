0fcd2b02f3e3629fc89aefb6a661c42c79463838
Gordon Pendleton <wgordonw1@gmail.com>
2020-07-01 10:40:13 -0400

    added comment and reference about case handled by register

--- tenant_schemas_celery/registry.py
+++ tenant_schemas_celery/registry.py
@@ -8,6 +8,8 @@ from .task import TenantTask
 class TenantTaskRegistry(TaskRegistry):
     def register(self, task):
         if inspect.isclass(task) and not issubclass(task, TenantTask):
+            # HANDLE AUTOMATIC REGISTRATION OF LEGACY CLASS BASED TASKS
+            # https://github.com/celery/celery/blob/v4.4.6/celery/task/base.py#L117
             class DynamicTenantTask(task, TenantTask):
                 name = task.name
             task = DynamicTenantTask
