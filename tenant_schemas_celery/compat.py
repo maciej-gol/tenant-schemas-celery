b2d05337035ac16ede0591d70edaf58652fede2e
Maciej Gol <1kroolik1@gmail.com>
2023-03-25 11:15:15 +0100

    Dependencies have been updated. Support for tenant-schemas and celery <5 has been dropped.

--- tenant_schemas_celery/compat.py
+++ tenant_schemas_celery/compat.py
@@ -1,19 +1,8 @@
-try:
-    from django_tenants.test.cases import TenantTestCase
-    from django_tenants.models import TenantMixin
-    from django_tenants.utils import (
-        get_public_schema_name,
-        get_tenant_model,
-        schema_context,
-        tenant_context,
-    )
-
-except ImportError:
-    from tenant_schemas.test.cases import TenantTestCase
-    from tenant_schemas.models import TenantMixin
-    from tenant_schemas.utils import (
-        get_public_schema_name,
-        get_tenant_model,
-        schema_context,
-        tenant_context,
-    )
+from django_tenants.test.cases import TenantTestCase
+from django_tenants.models import TenantMixin
+from django_tenants.utils import (
+    get_public_schema_name,
+    get_tenant_model,
+    schema_context,
+    tenant_context,
+)
