ad4aba2130bcbdb0dffab067e68e0281e0c2ea34
Maciej Gol <mg@github.com>
2024-09-30 22:36:59 +0200

    Init

---
+++ tenant_schemas_celery/db_scheduler.py
@@ -0,0 +1,76 @@
+from datetime import datetime
+from typing import List
+from django_celery_beat.models import PeriodicTask, PeriodicTasks
+from django_celery_beat import querysets
+from django_celery_beat.schedulers import DatabaseScheduler, ModelEntry
+
+from tenant_schemas_celery.compat import get_tenant_model, schema_context, get_public_schema_name, tenant_context
+
+class TenantAwareModelEntry(ModelEntry):
+    def __init__(self, model: "TenantAwareModelWrapper", app=None) -> None:
+        super().__init__(model.wrapped_model, app)
+        self.options["headers"]["_schema_name"] = model.schema_name
+
+
+class TenantAwareModelWrapper:
+    def __init__(self, schema_name: str, model: PeriodicTask) -> None:
+        self.name = f"{model.name}@{schema_name}"
+        self.schema_name = schema_name
+        self.wrapped_model = model
+
+class TenantAwareModelManager:
+    def get_public_schema_name(self) -> List[str]:
+        return [get_public_schema_name()]
+
+    def get_tenant_schema_names(self) -> List[str]:
+        return list(get_tenant_model().objects.values_list("schema_name", flat=True))
+
+    def get_schema_names(self) -> List[str]:
+        return [
+            *self.get_public_schema_name(),
+            *self.get_tenant_schema_names(),
+        ]
+
+    def enabled(self) -> List["TenantAwareModelWrapper"]:
+        models = []
+        for schema_name in self.get_schema_names():
+            with schema_context(schema_name):
+                models.extend([
+                    TenantAwareModelWrapper(schema_name=schema_name, model=model)
+                    for model in PeriodicTask.objects.enabled()
+                ])
+
+        return models
+
+class TenantAwarePeriodicTaskWrapper:
+    objects = TenantAwareModelManager()
+
+
+class TenantAwarePeriodicTasks:
+    @classmethod
+    def update_changed(cls, **kwargs):
+        PeriodicTasks.update_changed(**kwargs)
+
+    @classmethod
+    def last_change(cls) -> bool:
+        with schema_context(get_public_schema_name()):
+            all_tenants = list(get_tenant_model().objects.all())
+
+            last_change = PeriodicTasks.last_change()
+
+        for tenant in all_tenants:
+            with tenant_context(tenant):
+                tenant_last_change = PeriodicTasks.last_change()
+                if last_change:
+                    if tenant_last_change:
+                        last_change = max(last_change, tenant_last_change)
+                else:
+                    last_change = tenant_last_change
+
+        return last_change
+
+
+class TenantAwareDatabaseScheduler(DatabaseScheduler):
+    Entry = TenantAwareModelEntry
+    Model = TenantAwarePeriodicTaskWrapper
+    Changes = TenantAwarePeriodicTasks
